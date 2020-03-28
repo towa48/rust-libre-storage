a0c3a5f7a1ff4b8c7400dfadb7fb47179a590184
Anton Prilepsky <towa48@gmail.com>
2020-03-28 17:13:51 +0300

    Add json serializer to rocket

--- src/auth.rs
+++ src/auth.rs
@@ -1,4 +1,25 @@
-#[get("/token")]
-pub fn token() -> &'static str {
-    "Hello, world!"
+use rocket_contrib::json::Json;
+
+#[derive(Serialize, Deserialize)]
+pub struct Credentials<'a> {
+    user: &'a str,
+    password: &'a str,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TokenResponse<'a> {
+    token: &'a str,
+}
+
+impl<'a> TokenResponse<'a> {
+    fn new(token: &str) -> TokenResponse {
+        TokenResponse { token: token }
+    }
+}
+
+#[post("/token", format = "json", data = "<request>")]
+pub fn token(request: Json<Credentials>) -> Json<TokenResponse> {
+    // TODO: should we copy string here?
+    let response = TokenResponse::new(&request.user);
+    Json(response)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
 #[macro_use] extern crate diesel;
 extern crate dotenv;
 
